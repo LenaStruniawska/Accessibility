02a975edcef0b901499ce5e772a26e3f7b309ca5
Magdalena Struniawska <mag.sok@onet.pl>
2022-05-11 10:07:47 +0200

    Initial Commit

---
+++ AccessibilitySandbox/AccessibilitySandboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccessibilitySandboxApp.swift
+//  AccessibilitySandbox
+//
+//  Created by Slawek on 11/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct AccessibilitySandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
